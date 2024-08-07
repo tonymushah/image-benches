bb9516397a92cce9b3440612600503e9b7751811
tonymushah <tonyramanantsalama@gmail.com>
2024-08-07 12:38:54 +0300

    added image bench with rayon

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,33 @@
-use std::fs::{create_dir_all, read_dir};
+use image::ImageFormat;
+use image_benches::ConvertToParam;
+use rayon::prelude::*;
+use std::{
+    fs::{create_dir_all, read_dir},
+    path::Path,
+};
 
 fn main() -> anyhow::Result<()> {
     let input_dir = "./images";
-    let output_dir = "./output/jpeg";
-    create_dir_all(output_dir)?;
-    for img_path in read_dir(input_dir)?.flatten().map(|entry| entry.path()) {
-        println!("filename = {}", img_path.to_str().unwrap_or_default());
-        let image_ = image::open(&img_path)?;
-        image_.save_with_format(
-            format!(
-                "{output_dir}/{}.jpeg",
-                img_path
-                    .file_name()
-                    .and_then(|name| name.to_str())
-                    .ok_or(anyhow::Error::msg("Cannot get the filename"))?
-            ),
-            image::ImageFormat::Jpeg,
-        )?;
-    }
+    let output_dir = Path::new("./output/").to_path_buf();
+    let all_format = ImageFormat::all().collect::<Vec<_>>();
+    create_dir_all(&output_dir)?;
+    read_dir(input_dir)?
+        .flatten()
+        .map(|entry| entry.path())
+        .collect::<Vec<_>>()
+        .into_par_iter()
+        .for_each(move |file| {
+            let output_dir = output_dir.clone();
+            all_format.clone().into_par_iter().for_each(move |format| {
+                let con = ConvertToParam {
+                    file: file.clone(),
+                    format,
+                    output_dir: output_dir.clone(),
+                };
+                if let Err(e) = con.start() {
+                    eprintln!("{e}");
+                };
+            });
+        });
     Ok(())
 }
